b4a06072ae3813a0d80a1fbde97da57dbe1b2719
One MobiKwik Systems <MobiKwikSystems@Ones-MacBook-Pro.local>
2014-03-18 03:17:32 +0530

    Committing other files

---
+++ Share InfoTests/Share_InfoTests.m
@@ -0,0 +1,34 @@
+//
+//  Share_InfoTests.m
+//  Share InfoTests
+//
+//  Created by One MobiKwik Systems on 15/03/14.
+//  Copyright (c) 2014 InfoAssembly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Share_InfoTests : XCTestCase
+
+@end
+
+@implementation Share_InfoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
